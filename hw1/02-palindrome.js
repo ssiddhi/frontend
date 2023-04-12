09aff4d81e5058533e60ee0f36bdf646fe1dfef1
Shreshtha <ssiddhi@pdx.edu>
2023-04-11 23:21:10 -0700

    added code to check if input number is positive and palindrome in hw1/02-palindrome.js

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -16,25 +16,42 @@ const paragraph = document.createElement("p");
 // div.appendChild(paragraph);
 document.getElementsByClassName("result").appendChild(paragraph);
 
+paragraph.className = "para";
+
+//reference --- https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/input_event
+//reference ---https://www.geeksforgeeks.org/how-to-check-whether-a-passed-string-is-palindrome-or-not-in-javascript/
+
 function handleInput(e) {
-  //reference --- https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/input_event
   const inp = e.target.value;
-  //   paragraph.innerHTML = "Yes. This is a palindrome!";
-  // const len = e.length;
-  // const sum = 0;
-  //   let rem, temp;
-  //   const reverse = 0;
-  //   temp = e;
-  //   //performing reverse logic to check input number
-  //   while (e > 0) {
-  //     rem = e % 10;
-  //     e = parseInt(e / 10);
-  //     reverse = reverse * 10 + rem;
-  //   }
-  //   //to check if number is palindrome
-  //   if (reverse === temp) {
-  //     paragraph.innerHTML = "Yes. This is a palindrome!";
-  //   } else {
-  //     paragraph.innerHTML = "No. Try again.";
-  //   }
+
+  function check_palindrome(inp) {
+    //checking if input number is positive
+    if (e.target.value > 0) {
+      let j = inp.length - 1;
+
+      for (let i = 0; i < j / 2; i++) {
+        let x = str[i];
+        let y = str[j - i];
+        if (x != y) {
+          return false;
+        }
+        return true;
+      }
+    }
+  }
+
+  function is_palindrome(e) {
+    let res = check_palindrome(inp);
+    if (res === true) {
+      paragraph.innerHTML = "Yes. This is a palindrome!";
+    } else {
+      paragraph.innerHTML = "No. Try again.";
+    }
+  }
+
+  let test = 123;
+  is_palindrome(test);
+
+  //   document.getElementsByClassName("para").innerHTML =
+  //     "Yes. This is a palindrome!";
 }
