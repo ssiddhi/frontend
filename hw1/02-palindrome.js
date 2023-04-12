492806d9d411f962076087f9a43f9bb7dfafc499
Shreshtha <ssiddhi@pdx.edu>
2023-04-12 08:32:25 -0700

    removed unnecessary code from  hw1/02-palindrome.js

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -2,18 +2,10 @@ const elem = document.querySelector("input");
 
 elem.addEventListener("input", handleInput);
 
-//to display message, create a new div element to include result.
-//reference - CSS diner game
-// const div = document.querySelector("div :nth-child(3)");
-
-//giving class name to newly created div to append it as last child
-// div.className = "result";
-
 //creating a HTML paragraph element
 const paragraph = document.createElement("p");
 
 //appending paragraph as child element of div
-// div.appendChild(paragraph);
 document.getElementById("result").appendChild(paragraph);
 
 paragraph.className = "para";
@@ -30,22 +22,11 @@ function handleInput(e) {
   let halfLen = Math.floor(inp.length / 2);
   let checkPalindrome = true;
 
-  // //check if input is a positive number
-  // if (e.target.value > 0) {
-  //   let j = inp.length - 1;
-  //   for (let i = 0; i < halfLen; i++) {
-  //     if (inp[i] != inp[j]) {
-  //       checkPalindrome = false;
-  //     }
-  //     j -= 1;
-  //   }
-  // }
-
   //check if input is a positive number
   if (e.target.value > 0) {
     let j = inp.length - 1;
     for (let i = 0; i < halfLen; i++) {
-      if (inp[i] != inp[j - i]) {
+      if (inp[i] !== inp[j - i]) {
         checkPalindrome = false;
       }
     }
