bd86684c7fab8389ba8c9603c84b26a3346be897
Shreshtha <ssiddhi@pdx.edu>
2023-04-24 00:03:32 -0700

    added find and highlight text code in hw2/03-count.js

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,18 @@
 // Add your code here
 
-input.addEventListener('keydown', handleKeyDown);
+// reference-----https://tomekdev.com/posts/highlight-text-in-javascript
+
+let paragraphID = document.getElementById("paragraph");
+let inputID = document.getElementById("inputLabel");
+
+inputID.addEventListener("input", (event) => {
+  const searchText = event.target.value;
+  const regex = new RegExp(searchText, "gi");
+  let text = paragraphID.innerHTML;
+  text = text.replace(/(<mark>|<\/mark>)/gim, "");
+
+  const newText = text.replace(regex, "<mark>$&</mark>");
+  paragraphID.innerHTML = newText;
+});
+
+// input.addEventListener("keydown", handleKeyDown);
