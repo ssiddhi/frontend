376b06f61f2c6f0f2fd4d689efd2f76c48ce2fbd
Shreshtha <ssiddhi@pdx.edu>
2023-04-25 23:28:04 -0700

    added javascript code to grab all user input and print to console in  hw2/05-form.js

--- hw2/05-form.js
+++ hw2/05-form.js
@@ -1,1 +1,61 @@
 // Add your code here
+// reference---https://stackoverflow.com/questions/11599666/get-the-value-of-checked-checkbox
+
+let formInput = document.getElementById("form");
+
+formInput.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  let formData = new FormData(event.target);
+
+  let user = formData.get("name");
+  let email = formData.get("email");
+
+  let rstatus = document.getElementById("options").value;
+
+  let course = document.querySelectorAll('input[name="courselist"]:checked');
+  //   let message = formData.get("textArea");
+  let message = document.getElementById("textArea").value;
+
+  //printing user input data to console
+  //name
+  if (user === null || user === "") {
+    console.log("Full Name: No submission");
+  } else {
+    console.log("Full Name: " + user);
+  }
+
+  //email
+  if (email === null || email === "") {
+    console.log("Email: No submission");
+  } else {
+    console.log("Email: " + email);
+  }
+
+  //registration status
+  if (rstatus === null || rstatus === "") {
+    console.log("Registration Status: No submission");
+  } else {
+    console.log("Registration Status: " + rstatus);
+  }
+
+  //checklist
+  let checkboxesChecked = [];
+
+  // loop over them all
+  course.forEach((checkbox) => {
+    // And stick the checked ones onto an array...
+    checkboxesChecked.push(checkbox.value);
+  });
+
+  console.log("Courses: ", checkboxesChecked);
+
+  //message
+  if (message === null || message === "") {
+    console.log("Message: No submission");
+  } else {
+    console.log("Message: " + message);
+  }
+}
