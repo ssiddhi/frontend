ef7301aeb394dbafe945e7825bce08ed5413f39e
Shreshtha <ssiddhi@pdx.edu>
2023-04-29 12:22:02 -0700

    applied suggested changes, timeinterval is taken as user input in hw2/04-background.js

--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,43 +1,41 @@
 // Add your code here
 
-//reference---- https://www.codespeedy.com/how-to-change-background-color-every-seconds-in-javascript/
-//reference---- https://codepen.io/jagadeshanh/pen/YGJmmv
-//reference---- https://developer.mozilla.org/en-US/docs/Web/API/setInterval
-//reference---- https://stackoverflow.com/questions/74280613/how-to-change-background-color-randomly-in-a-javascript-function
-
-let target = document.querySelector("body");
+let interval;
 let toggle = document.querySelector(".btn");
-let userInput = document.querySelector("input");
-toggle.addEventListener("click", changeBgColor);
 
-setInterval(changeBgColor, 3000);
+toggle.addEventListener("click", handleClick);
 
-//set the background color to a randomly generated color for a given time interval.
-function getRandomColor() {
-  const r = Math.floor(Math.random() * 255);
-  const g = Math.floor(Math.random() * 255);
-  const b = Math.floor(Math.random() * 255);
-  return `rgba(${r}, ${g}, ${b})`;
-}
+function handleClick(event) {
+  event.preventDefault();
+  let input = document.querySelector("#userInput");
 
-function changeBgColor() {
-  let newColor = getRandomColor();
-  target.style.backgroundColor = newColor;
   setToggleStyle();
-}
 
-// button that allows the user to “stop” and “start” the background color change.
-function setToggleStyle() {
-  if (toggle.value === "Start") {
-    toggle.value = "Stop";
-    toggle.classList = "btn btn-danger mb-2";
-  } else {
-    toggle.value === "Stop";
-    changeInterval();
+  function setToggleStyle() {
+    if (toggle.value === "Start") {
+      toggle.value = "Stop";
+      toggle.classList = "btn btn-danger mb-2";
+      interval = setInterval(() => {
+        // function getRandomColor() {
+        const r = Math.floor(Math.random() * 255);
+        const g = Math.floor(Math.random() * 255);
+        const b = Math.floor(Math.random() * 255);
+        let bgColor = `rgba(${r}, ${g}, ${b})`;
+        document.body.style.background = bgColor;
+        // return;
+      }, input.value * 1000);
+      input.disabled = true;
+      // input.disabled = true;
+      // setInterval();
+      return;
+    } else {
+      toggle.value = "Start";
+      toggle.classList = "btn btn-primary mb-2";
+      clearInterval(interval);
+      interval = null;
+      document.body.style.background = "white";
+      input.disabled = false;
+      // changeInterval();
+    }
   }
-}
-
-// specify a new interval for changing the background color
-function changeInterval() {
-  setInterval(changeBgColor, 1000);
 }
