b08383f9cc214b6ebb6635481b3c34a9f553ed2b
Shreshtha <ssiddhi@pdx.edu>
2023-04-30 20:51:51 -0700

    applied suggested changes, removed commented code in hw2/05-form.js

--- hw2/05-form.js
+++ hw2/05-form.js
@@ -14,43 +14,43 @@ function handleSubmit(event) {
   let rstatus = document.getElementById("options").value;
 
   let course = document.querySelectorAll('input[name="courselist"]:checked');
-  //   let message = formData.get("textArea");
+
   let message = document.getElementById("textArea").value;
 
   //printing user input data to console
-  //name
+  //print user name
   if (user === null || user === "") {
     console.log("Full Name: No submission");
   } else {
     console.log("Full Name: " + user);
   }
 
-  //email
+  //print user email
   if (email === null || email === "") {
     console.log("Email: No submission");
   } else {
     console.log("Email: " + email);
   }
 
-  //registration status
+  //print user registration status
   if (rstatus === null || rstatus === "") {
     console.log("Registration Status: No submission");
   } else {
     console.log("Registration Status: " + rstatus);
   }
 
-  //checklist
+  //create an array to store the checklist
   let checkboxesChecked = [];
 
   // loop over them all
   course.forEach((checkbox) => {
-    // And stick the checked ones onto an array...
+    // And stick the checked ones onto the array
     checkboxesChecked.push(checkbox.value);
   });
 
   console.log("Courses: ", checkboxesChecked);
 
-  //message
+  //print user message
   if (message === null || message === "") {
     console.log("Message: No submission");
   } else {
