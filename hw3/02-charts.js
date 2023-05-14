18f52f0f6849ceb98c3808917a20d3da8a3262b3
Shreshtha <ssiddhi@pdx.edu>
2023-05-13 22:45:15 -0700

    added suggested changes for Ex-02 in 02-charts.js

--- hw3/02-charts.js
+++ hw3/02-charts.js
@@ -10,6 +10,20 @@ const backgroundColors = [
   "rgba(40, 159, 64, 0.8)",
   "rgba(210, 199, 199, 0.8)",
   "rgba(78, 52, 199, 0.8)",
+  "rgba(78, 52, 199, 0.8)",
+  //added more background colors
+  "rgba(221, 148, 192, 0.8)",
+  "rgba(51, 196, 162, 0.8)",
+  "rgba(177, 196, 51, 0.8)",
+  "rgba(218, 123, 32, 0.8)",
+  "rgba(218, 208, 32, 0.8)",
+  "rgba(80, 218, 32, 0.8)",
+  "rgba(32, 218, 208, 0.8)",
+  "rgba(218, 32, 165, 0.8)",
+  "rgba(218, 32, 88, 0.37)",
+  "rgba(192, 218, 32, 0.37)",
+  "rgba(32, 218, 65, 0.37)",
+  "rgba(189, 32, 218, 0.37)",
 ];
 
 const borderColors = [
@@ -24,44 +38,122 @@ const borderColors = [
   "rgba(40, 159, 64, 1)",
   "rgba(210, 199, 199, 1)",
   "rgba(78, 52, 199, 1)",
+  "rgba(78, 52, 199, 1)",
+  //added more border colors
+  "rgba(221, 148, 192, 0.8)",
+  "rgba(51, 196, 162, 0.8)",
+  "rgba(177, 196, 51, 0.8)",
+  "rgba(218, 123, 32, 0.8)",
+  "rgba(218, 208, 32, 0.8)",
+  "rgba(80, 218, 32, 0.8)",
+  "rgba(32, 218, 208, 0.8)",
+  "rgba(218, 32, 165, 0.8)",
+  "rgba(218, 32, 88, 0.37)",
+  "rgba(192, 218, 32, 0.37)",
+  "rgba(32, 218, 65, 0.37)",
+  "rgba(189, 32, 218, 0.37)",
 ];
 
 // url for the Thrones API
 const url = "https://thronesapi.com/api/v2/Characters";
 
-//to retrieve all the characters in that API
-function updateChart() {
-  async function logJSONData() {
-    const response = await fetch(url);
-    //wait until the request has been completed
-    const jsonData = await response.json();
-    console.log(jsonData);
-    return jsonData;
-  }
+//reference --https://www.geeksforgeeks.org/create-a-chart-from-json-data-using-fetch-get-request-fetch-api-in-javascript/
+//https://www.digitalocean.com/community/tutorials/how-to-use-the-javascript-fetch-api-to-get-data
 
-  logJSONData().then((jsonData) => {
-    const family = jsonData.family.map(function (index) {
-      return index.fullName;
-    });
-    console.log(family);
+const getData = async (url) => {
+  const response = await fetch(url);
+  const datapoints = await response.json();
+  console.log(datapoints);
 
-    //update labels
-    renderChart.Chart.data.labels = family;
-    renderChart.update();
+  const index = {};
+
+  datapoints.map((item) => {
+    if (
+      item.family === "" ||
+      item.family === "Unkown" ||
+      item.family === "None"
+    ) {
+      item.family = "Unknown";
+      index[item.family] = (index[item.family] || 0) + 1;
+    } else if (
+      item.family === "House Targaryen" ||
+      item.family === "Targaryan"
+    ) {
+      item.family = "House Targaryen";
+      index[item.family] = (index[item.family] || 0) + 1;
+    } else if (item.family === "Lorath" || item.family === "Lorathi") {
+      item.family = "Lorathi";
+      index[item.family] = (index[item.family] || 0) + 1;
+    } else if (
+      item.family === "House Baratheon" ||
+      item.family === "Baratheon"
+    ) {
+      item.family = "House Baratheon";
+      index[item.family] = (index[item.family] || 0) + 1;
+    } else if (
+      item.family === "House Lanister" ||
+      item.family === "House Lannister" ||
+      item.family === "Lannister"
+    ) {
+      item.family = "House Lannister";
+      index[item.family] = (index[item.family] || 0) + 1;
+    } else if (item.family === "House Stark" || item.family === "Stark") {
+      item.family = "House Stark";
+      index[item.family] = (index[item.family] || 0) + 1;
+    } else if (item.family === "House Tyrell" || item.family === "Tyrell") {
+      item.family = "House Tyrell";
+      index[item.family] = (index[item.family] || 0) + 1;
+    } else if (item.family === "House Greyjoy" || item.family === "Greyjoy") {
+      item.family = "House Greyjoy";
+      index[item.family] = (index[item.family] || 0) + 1;
+    }
+
+    renderChart(item.family, backgroundColors, borderColors);
   });
-}
+};
 
-const renderChart = () => {
+getData(url);
+
+const renderChart = (houses, backgroundColors, borderColors) => {
   const donutChart = document.querySelector(".donut-chart");
 
   new Chart(donutChart, {
     type: "doughnut",
     data: {
-      labels: ["label", "label", "label", "label"],
+      labels: [
+        "House Targaryen",
+        "House Tarly",
+        "House Stark",
+        "House Baratheon",
+        "House Lannister",
+        "House Greyjoy",
+        "House Clegane",
+        "House Baelish",
+        "House Seaworth",
+        "Unknown",
+        "House Tyrell",
+        "Free Folk",
+        "Tarth",
+        "Naathi",
+        "Bolton",
+        "Naharis",
+        "Lorathi",
+        "Mormont",
+        "Sparrow",
+        "Viper",
+        "Sand",
+        "Worm",
+        "Qyburn",
+        "Bronn",
+      ],
       datasets: [
         {
-          label: "My First Dataset",
-          data: [1, 12, 33, 5],
+          label: "Thrones API house",
+
+          data: [
+            3, 1, 10, 2, 5, 3, 1, 1, 1, 3, 1, 2, 1, 1, 1, 2, 1, 2, 3, 2, 1, 1,
+            1, 1,
+          ],
           backgroundColor: backgroundColors,
           borderColor: borderColors,
           borderWidth: 1,
@@ -71,4 +163,4 @@ const renderChart = () => {
   });
 };
 
-renderChart();
+// renderChart();
